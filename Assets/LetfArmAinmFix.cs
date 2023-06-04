e0f2de5e2ffb513c594d50237114ee7bc22e0c0d
WRXinYue <3322543587@qq.com>
2023-06-04 17:56:06 +0800

    feat

---
+++ Assets/LetfArmAinmFix.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetfArmAinmFix : MonoBehaviour
+{
+    private Animator anim;
+
+    public Vector3 a;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+    private void OnAnimatorIK()
+    {
+        if (anim.GetBool("defense") == false)
+        {
+        Transform leftLowerArm = anim.GetBoneTransform(HumanBodyBones.LeftLowerArm);
+        leftLowerArm.localEulerAngles += 0.75f * a;
+        anim.SetBoneLocalRotation(HumanBodyBones.LeftLowerArm,Quaternion.Euler(leftLowerArm.localEulerAngles));
+        }
+    }
+}
